96ccca1fdf0adc3c0a74549528e0d34efcb2ee7a
DeathbatO13 <danielalejo99@gmail.com>
2025-10-14 13:03:48 -0500

    Recuperacion Controller

--- src/main/java/com/sistema/ui/RecuperacionController.java
+++ src/main/java/com/sistema/ui/RecuperacionController.java
@@ -1,4 +1,35 @@
 package com.sistema.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RecuperacionController {
+
+    @FXML
+    private TextField correoTextField, codigoVerificacion;
+
+    @FXML
+    private PasswordField primeraContra, segundaContra;
+
+    @FXML
+    private Button btnEnviarCorreo, btnVerificarCodigo, btnConfirmarCambio;
+
+    @FXML
+    private Label estadoCodigo, estadoContra, cambioExistoso;
+
+    @FXML
+    public void btnEnviarCorreoAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btnVerificarAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btnConfirmarAction(ActionEvent actionEvent) {
+    }
 }
